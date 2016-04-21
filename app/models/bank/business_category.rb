ad1dd652bc2f791de5b343cd743b359862a86262
fushang318 <fushang318@gmail.com>
2016-04-21 18:02:11 +0800

    去掉business_category name 不能重复的校验

--- app/models/bank/business_category.rb
+++ app/models/bank/business_category.rb
@@ -8,7 +8,7 @@ module Bank
     field :name,   type: String
     field :number, type: String
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
 
     default_scope ->{order(:id.asc)}
 
