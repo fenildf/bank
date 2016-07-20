b7495518bc02f2672b1ce521540395dc0deeffe3
fushang318 <fushang318@gmail.com>
2016-07-20 16:50:12 +0800

    Update engine.rb

--- lib/bank/engine.rb
+++ lib/bank/engine.rb
@@ -4,10 +4,6 @@ module Bank
     config.to_prepare do
       ApplicationController.helper ::ApplicationHelper
 
-      Dir.glob(Rails.root + "app/decorators/bank/**/*_decorator.rb").each do |c|
-        require_dependency(c)
-      end
-
       if defined? EnterprisePositionLevel
         EnterprisePositionLevel::Post.class_eval do
           has_and_belongs_to_many :business_categories, class_name: "Bank::BusinessCategory"
@@ -17,6 +13,11 @@ module Bank
           has_and_belongs_to_many :enterprise_posts, class_name: "EnterprisePositionLevel::Post"
         end
       end
+      
+      Dir.glob(Rails.root + "app/decorators/bank/**/*_decorator.rb").each do |c|
+        require_dependency(c)
+      end
+      
     end
   end
 end
